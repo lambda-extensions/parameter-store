630a455880a658806224598ee4678cd0c940a9b8
Joe Snell <joe.snell@daveramsey.com>
2020-10-08 16:24:54 -0500

    fix pipfile

--- python/bin/build.sh
+++ python/bin/build.sh
@@ -6,7 +6,6 @@ set -e
 [ -f extension.zip ] && rm extension.zip
 
 pipenv lock -r > requirements.txt
-pip install -r requirements.txt -t build
 
 chmod +x extensions/parameter-store-extension
 chmod +x parameter-store-extension/extension.py
@@ -15,5 +14,7 @@ mkdir -p build/
 
 cp -R extensions build/
 cp -R parameter-store-extension build/
+
+pip install -r requirements.txt -t build/parameter-store-extension
 
 cd build; zip -r ../extension.zip *
